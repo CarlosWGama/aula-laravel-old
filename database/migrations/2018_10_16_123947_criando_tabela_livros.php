bbc43bed37d2ef18f1b48b8f4650524951e90326
Carlos W. Gama <carloswgama@gmail.com>
2018-10-16 10:00:42 -0300

    Migrações

--- database/migrations/2018_10_16_123947_criando_tabela_livros.php
+++ database/migrations/2018_10_16_123947_criando_tabela_livros.php
@@ -4,22 +4,21 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersTable extends Migration
+class CriandoTabelaLivros extends Migration
 {
     /**
      * Run the migrations.
      *
      * @return void
      */
-    public function up()
-    {
-        Schema::create('users', function (Blueprint $table) {
+    public function up() {
+        Schema::create('livros', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->bigInteger('isbn');
+            $table->string('titulo');
+            $table->string('autor');
+            $table->text('resumo');
+            $table->integer('categoria');
             $table->timestamps();
         });
     }
@@ -31,6 +30,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('livros');
     }
 }
