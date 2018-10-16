98dc7e94ccc9a7ef05b44ee09e4208d9196a5a30
Carlos W. Gama <carloswgama@gmail.com>
2018-10-16 10:47:59 -0300

    Adicionado Models

--- app/Http/Controllers/LivrosController.php
+++ app/Http/Controllers/LivrosController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Livro;
 
 class LivrosController extends Controller {
     
@@ -18,32 +19,22 @@ class LivrosController extends Controller {
             'resumo'=> 'required'
         ]);
 
+        Livro::create($request->all());
+
         return redirect()->route('livros.listar')->with('acao', 'Cadastrado com sucesso');
     }
 
     public function listar() {
-        $dados['livros'] = [
-            ['id' => 1, 'isbn' => '999999999', 'autor' => 'Autor 1', 'titulo' => 'Livro 1'],
-            ['id' => 2, 'isbn' => '888888888', 'autor' => 'Autor 2', 'titulo' => 'Livro 2']
-        ];
+        $dados['livros'] = Livro::all();
         return view('livros.listar', $dados);
     }
 
     public function editar($id) {
-        $dados = ['livro' => [
-                'id'        => 1,
-                'isbn'      => 23123123123,
-                'categoria' => 3,
-                'titulo'    => 'Titulo 1',
-                'autor'     => 'Autor 1',
-                'resumo'    => 'Bla bla bla bla bla bla'
-            ]
-        ];
-        
+        $dados = ['livro' => Livro::find($id)];
         return view('livros.edicao', $dados);
     }
     
-    public function atualizar(Request $request) {
+    public function atualizar(Request $request, $id) {
         $request->validate([
             'isbn'  => 'required|integer',
             'titulo'=> 'required',
@@ -51,22 +42,18 @@ class LivrosController extends Controller {
             'resumo'=> 'required'
         ]);
 
+        Livro::where('id', $id)->update($request->except('_token'));
+
         return redirect()->route('livros.listar')->with('acao', 'Atualizado com sucesso');
     }
 
     public function visualizar($id) {
-        $dados = ['livro' => [
-                    'id'        => 1,
-                    'titulo'    => 'Titulo 1',
-                    'autor'     => 'Autor 1',
-                    'resumo'    => 'Bla bla bla bla bla bla'
-                ]
-        ];
+        $dados = ['livro' => Livro::find($id)];
         return view('livros.visualizar', $dados);
     }
 
     public function excluir($id) {
-     
+        Livro::destroy($id);
         return redirect()->route('livros.listar')->with('acao', 'Excluído com sucesso');
     }
 }
